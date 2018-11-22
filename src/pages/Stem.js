04d1c44f4c64da864067de13aed8029d5e763a6f
Nick Bowers <greennick7777@gmail.com>
2018-11-21 22:40:25 -0500

    adds filler text for home pages

--- src/pages/Stem.js
+++ src/pages/Stem.js
@@ -4,6 +4,11 @@ import BasicSection from '../organisms/BasicSection'
 import Navigation from '../organisms/Navigation'
 import PageIndicator from '../atoms/PageIndicator'
 
+const sectionContent = {
+  heading: 'What is STEM?',
+  paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac posuere metus. Praesent aliquet nulla quis ligula pharetra, vestibulum posuere arcu pretium. Integer et nisl fermentum, gravida mi ac, elementum eros. Phasellus pharetra consequat libero non faucibus. Nullam hendrerit imperdiet ipsum, eget tincidunt diam pharetra eu. Mauris sit amet lorem sed nibh placerat volutpat. Morbi suscipit aliquet augue ac fermentum. Etiam lacinia justo sit amet turpis dictum, auctor aliquet elit convallis. Nam hendrerit lorem non neque accumsan, quis aliquet orci tempus. Sed in condimentum magna. Ut maximus ipsum eget libero blandit rhoncus.'
+}
+
 class Tsa extends Component {
   constructor (props) {
     super (props)
@@ -33,7 +38,7 @@ class Tsa extends Component {
 
     return (
       <main onWheel={this.handleWheel} className='gradient-bg' >
-        <BasicSection heading='What is STEM?' />
+        <BasicSection heading={sectionContent.heading} paragraph={sectionContent.paragraph} />
         <PageIndicator />
         <Navigation />
       </main>
