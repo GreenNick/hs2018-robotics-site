fc0ac7d698bee7814d4097c2a9a1cae978dc114e
Nick Bowers <greennick7777@gmail.com>
2018-12-05 10:41:44 -0500

    minor tweaks

--- src/Components/HomePage/Join/SectionDues/SectionDues.js
+++ src/Components/HomePage/Join/SectionDues/SectionDues.js
@@ -6,7 +6,7 @@ const SectionDues = props => {
   return (
     <section className={styles.container}>
       <h3>Dues owed: ${props.subtotal}</h3>
-      <p>Dues must be payed in person to Mr. Hicks or a TSA Officer. After submitting, your information will be recorded, and you will receive an email detailing any further steps you need to take. Completing this form will not make you a member of your selected programs, it just lets us know that you are interested. </p>
+      <p>Dues must be payed in person to Mr. Hicks or a TSA Officer. After submitting, your information will be recorded, and you will receive an email detailing any further steps you need to take. Completing this form will not make you a member of your selected programs; it just lets us know that you are interested. </p>
     </section>
   )
 }
