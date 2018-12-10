07669bdcc7dead20f5f126ea79d5f3b8b521e7e5
Nick Bowers <greennick7777@gmail.com>
2018-12-10 13:53:29 -0500

    partially adds contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react'
 import Navigation from '../Navigation/Navigation'
+import styles from './Contact.module.css'
 
 class Contact extends Component {
   render () {
     return (
       <main>
+        <section className={styles.container}>
+          <h2>Contact</h2>
+          <div className={styles.col1}>
+            <h3>Send us a message</h3>
+            <form>
+              <label htmlFor='name'>Name</label>
+              <input
+                className={styles.input}
+                type='text'
+                id='name'
+                placeholder='Enter your full name' />
+              <label htmlFor='email'>Email</label>
+              <input
+                className={styles.input}
+                type='email'
+                id='email'
+                placeholder='Enter your email address' />
+              <label htmlFor='message'>Message</label>
+              <textarea
+                className={styles.input}
+                id='message'
+                placeholder='Tell us what is on your mind'
+                rows='12'></textarea>
+              <input type='submit' value='Submit' />
+            </form>
+          </div>
+          <div className={styles.col2}>
+            <h3>Tucker High School</h3>
+            <p>
+              5036 LaVista Rd <br />
+              Tucker, GA 30084 <br />
+              (678) 874-3702 <br />
+              tuckerrobotigers@gmail.com
+            </p>
+            <hr />
+            <div className={styles.map}></div>
+          </div>
+        </section>
         <Navigation />
       </main>
     )
