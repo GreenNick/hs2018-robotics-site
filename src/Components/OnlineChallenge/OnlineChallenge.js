a54a65210f460bb0535365498d51038de4f4b987
Nicholas Bowers <greennick7777@gmail.com>
2019-01-09 17:12:22 -0500

    adds alt text

--- src/Components/OnlineChallenge/OnlineChallenge.js
+++ src/Components/OnlineChallenge/OnlineChallenge.js
@@ -12,12 +12,12 @@ const OnlineChallenge = () => {
             <h2>Dell Website Challenge</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellendus aspernatur saepe itaque sint mollitia dolore, rem ratione reprehenderit harum quasi maiores corrupti, nisi voluptates beatae alias tenetur consequatur voluptate? Provident iusto, nam molestias saepe a ad atque tempora. Unde modi quam dignissimos! Error reiciendis ab, deserunt ad labore fugiat repellat vitae! Et pariatur odit molestiae, nesciunt animi sed exercitationem. Soluta beatae neque accusantium id numquam quasi, laboriosam ab facilis culpa, quibusdam impedit optio ea tempore eaque hic. Similique consectetur dolore totam reiciendis nulla corrupti incidunt consequuntur mollitia aperiam nihil?</p>
           </div>
-          <img />
+          <img src='#' alt='' />
         </section>
         <section className={styles.container}>
           <h2>Technologies</h2>
           <div className={styles.item}>
-            <img src='/Images/OnlineChallenge/HTML-CSS.png' alt='' />
+            <img src='/Images/OnlineChallenge/HTML-CSS.png' alt='HTML and CSS logo' />
             <div>
               <h3>HTML & CSS</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellendus aspernatur saepe itaque sint mollitia dolore, rem ratione reprehenderit harum quasi maiores corrupti, nisi voluptates beatae alias tenetur consequatur voluptate?
@@ -26,7 +26,7 @@ const OnlineChallenge = () => {
             </div>
           </div>
           <div className={styles.item}>
-            <img src='/Images/OnlineChallenge/JavaScript.png' alt='' />
+            <img src='/Images/OnlineChallenge/JavaScript.png' alt='JavaScript logo' />
             <div>
               <h3>JavaScript</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellendus aspernatur saepe itaque sint mollitia dolore, rem ratione reprehenderit harum quasi maiores corrupti, nisi voluptates beatae alias tenetur consequatur voluptate?
@@ -35,7 +35,7 @@ const OnlineChallenge = () => {
             </div>
           </div>
           <div className={styles.item}>
-            <img src='/Images/OnlineChallenge/React.png' alt='' />
+            <img src='/Images/OnlineChallenge/React.png' alt='React logo' />
             <div>
               <h3>React</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellendus aspernatur saepe itaque sint mollitia dolore, rem ratione reprehenderit harum quasi maiores corrupti, nisi voluptates beatae alias tenetur consequatur voluptate?
@@ -45,7 +45,7 @@ const OnlineChallenge = () => {
           </div>
         </section>
         <section>
-          <img />
+          <img src='#' alt='' />
           <div>
             <h2>Development Team</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellendus aspernatur saepe itaque sint mollitia dolore, rem ratione reprehenderit harum quasi maiores corrupti, nisi voluptates beatae alias tenetur consequatur voluptate? Provident iusto, nam molestias saepe a ad atque tempora. Unde modi quam dignissimos! Error reiciendis ab, deserunt ad labore fugiat repellat vitae! Et pariatur odit molestiae, nesciunt animi sed exercitationem. Soluta beatae neque accusantium id numquam quasi, laboriosam ab facilis culpa, quibusdam impedit optio ea tempore eaque hic. Similique consectetur dolore totam reiciendis nulla corrupti incidunt consequuntur mollitia aperiam nihil?</p>
