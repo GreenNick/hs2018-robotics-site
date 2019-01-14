6cc3864dc7556afb5e0e58fe7598ebc48b427502
Nicholas Bowers <greennick7777@gmail.com>
2019-01-14 16:57:27 -0500

    fixes date

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -12,7 +12,7 @@ const AboutUs = () => {
           <img src='#' alt='' />
           <div>
             <h2>Our School</h2>
-            <p>Tucker High is a public high school in DeKalb County, Georgia. The school was first established in 1918 and has since grown to 1,800 enrolled students. The student body is very diverse with a large immigrant population. We have a variety of advanced programs such as IB and STEM. Our school maintains a balance of academics, athletics, and extracurricular activities. We have both Vex and TSA after school clubs. We greatly value STEM subjects and always put in maximum effort when working on our projects.</p>
+            <p>Tucker High is a public high school in DeKalb County, Georgia. The school was first established in 1916 and has since grown to 1,800 enrolled students. The student body is very diverse with a large immigrant population. We have a variety of advanced programs such as IB and STEM. Our school maintains a balance of academics, athletics, and extracurricular activities. We have both Vex and TSA after school clubs. We greatly value STEM subjects and always put in maximum effort when working on our projects.</p>
             <p>The different STEM groups in our school have formed a community of their own. Many of us have been in the same classes together for years now and we have developed long-lasting friendships. We have grown attached to the programs we participate in due to the hours we have spent working together late into the night.</p>
           </div>
         </section>
