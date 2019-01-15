a784461626d524900ff79b7264a8376bf70fbeb7
1320D_Programmer <justinwadex@gmail.com>
2019-01-15 02:17:11 -0500

    Tcuker Tournament TSA

--- src/Components/TuckerTournament/TuckerTournament.js
+++ src/Components/TuckerTournament/TuckerTournament.js
@@ -23,9 +23,7 @@ const TuckerTournament = () => {
             <img src='/Images/TuckerTournament/TSA.jpg' alt='' />
             <div>
               <h3>Technology Student Association</h3>
-              <p>Justin - Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellendus aspernatur saepe itaque sint mollitia dolore, rem ratione reprehenderit harum quasi maiores corrupti, nisi voluptates beatae alias tenetur consequatur voluptate?
-              Provident iusto, nam molestias saepe a ad atque tempora. Unde modi quam dignissimos! Error reiciendis ab, deserunt ad labore fugiat repellat vitae! Et pariatur odit molestiae, nesciunt animi sed exercitationem.
-              Soluta beatae neque accusantium id numquam quasi, laboriosam ab facilis culpa, quibusdam impedit optio ea tempore eaque hic. Similique consectetur dolore totam reiciendis nulla corrupti incidunt consequuntur mollitia aperiam nihil?</p>
+              <p>At Tucker High School, our VEX Robotics program and our TSA chapter are closely interrelated. Therefore, TSA always helps to set up for the yearly tucker tournament. The TSA members are greatly appreciated when setting up, as many of them have technological expertise that can be helpful when setting up projectors or tournament management hardware. Additionally, their critical thinking skills are very helpful when confronting the many problems that come with setting up a tournament, such as efficient use of space or difficulty with wiring.</p>
             </div>
           </div>
           <div className={styles.clubs}>
